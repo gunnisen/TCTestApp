b8d49986369eed78450f38392428542648f1d51f
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-02 13:53:07 +0100

    Add project files.

---
+++ TCTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
